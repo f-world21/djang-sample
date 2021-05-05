70949a7ec848d8d9bfdaa8c41283b0cc6223dc2b
Satoshi Kobayashi <satoshi.forest@gmail.com>
2021-05-05 15:33:37 +0900

    requestの型を書く

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.http.response import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+
+def index(request: WSGIRequest):
     print(type(request))
-    return HttpResponse("Hello, world. You're at the polls index.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    return HttpResponse("Hello, world. You're at the polls index. ")
