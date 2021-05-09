01f9094ddfe5828cdd11830f4639f2dd8078cda2
Satoshi Kobayashi <satoshi.forest@gmail.com>
2021-05-09 18:06:00 +0900

    finished tutorial 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,49 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.http.response import HttpResponse
+from django.db import models
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Choice, Question
 
 # Create your views here.
 
 
-def index(request: WSGIRequest):
-    print(type(request))
-    return HttpResponse("Hello, world. You're at the polls index. ")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self) -> models.query.QuerySet:
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request: WSGIRequest, question_id: int):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting from.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
